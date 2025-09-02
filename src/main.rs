6842ba8549d5d045d4414b8753b4dc0871ffda71
qblmchmmddev <qblmchmmd.dev@gmail.com>
2025-09-02 07:33:53 +0700

    setup dhat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     println!("Hello, world!");
 }
